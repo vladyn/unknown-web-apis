b0675cc90768e6763ed46b2077fe5b3b4d85349f
Vladimir Varbanov <vladimir.varbanov@puzzel.com>
2023-01-03 12:44:27 +0200

    feature: Added date localization though the Intl.DateFormat

--- new-origin/main.js
+++ new-origin/main.js
@@ -1,6 +1,31 @@
 const broadcast = new BroadcastChannel("quote_channel");
 const quote = document.querySelector("#quote");
 
+
+const quotePlaceHolder = document.querySelector("#quote");
+const shareQuote = document.querySelector("#share-quote");
+
 broadcast.onmessage = ({data}) => {
+    quotePlaceHolder.innerHTML = data;
     quote.innerHTML = data;
 };
+
+shareQuote.addEventListener('click', () => {
+    shareData({
+        title: "A nice quote",
+        url: location.href,
+        text: quotePlaceHolder.textContent
+    }).then(res => {
+        console.log(res)
+    }).catch(error => {
+        console.error(error)
+    })
+});
+
+const shareData = async (data) => {
+    try {
+        await navigator.share(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
